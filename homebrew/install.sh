91bd4430d31377164ce349cd51bc334682bd1a9a
Egor Nizovets <jnesapps@gmail.com>
2024-10-02 00:54:04 +0300

    update

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,8 +5,11 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if [ "$(uname -s)" == "Darwin" ]
+  then
+    echo "  Installing Homebrew for you."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
 fi
 
 exit 0
